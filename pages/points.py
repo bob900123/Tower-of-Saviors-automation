e17eb94e03cc93b8cb9d1f238d459290fa61ced1
ChuangCH <bob900123@gmail.com>
2026-03-13 13:41:31 +0800

    修改儲存位置按鍵

--- pages/points.py
+++ pages/points.py
@@ -1,7 +1,7 @@
 import flet as ft
-import json
 
-from storage.data import items, table_data
+from storage.data import table_data
+
 
 def PointsPage(page: ft.Page):
     table = ft.DataTable(
@@ -9,7 +9,7 @@ def PointsPage(page: ft.Page):
             ft.DataColumn(ft.Text("名稱")),
             ft.DataColumn(ft.Text("x"), numeric=True),
             ft.DataColumn(ft.Text("y"), numeric=True),
-            ft.DataColumn(ft.Text("刪除")), 
+            ft.DataColumn(ft.Text("刪除")),
         ],
         width=350,
     )
@@ -20,20 +20,26 @@ def PointsPage(page: ft.Page):
             cells = []
             for j, val in enumerate(row):
                 txt = ft.Text(val)
+
                 def make_on_tap(i=i, j=j, txt=txt):
                     def on_tap(e):
-                        if j != 0: return
+                        if j != 0:
+                            return
                         tf = ft.TextField(value=txt.value, autofocus=True, expand=True)
+
                         def save(e2):
                             table_data[i][j] = tf.value
                             rebuild_table()
                             table.update()
+
                         tf.on_blur = save
                         tf.on_submit = save
                         cell.content = tf
                         cell.update()
+
                     cell = ft.DataCell(txt, on_tap=on_tap)
                     return cell
+
                 cell = make_on_tap()
                 cells.append(cell)
 
@@ -41,6 +47,7 @@ def PointsPage(page: ft.Page):
                 table_data.pop(idx)
                 rebuild_table()
                 table.update()
+
             cells.append(
                 ft.DataCell(
                     ft.IconButton(
@@ -53,13 +60,19 @@ def PointsPage(page: ft.Page):
             )
 
             table.rows.append(ft.DataRow(cells=cells))
+
     rebuild_table()
 
     return ft.Column(
         controls=[
             ft.Container(
-                content= ft.Text("按 w 鍵儲存位置", color="gray", size=20, weight=ft.FontWeight.BOLD),
-                margin= 10
+                content=ft.Text(
+                    "按 Ins 鍵儲存位置",
+                    color="gray",
+                    size=20,
+                    weight=ft.FontWeight.BOLD,
+                ),
+                margin=10,
             ),
             ft.Column(
                 controls=[table],
@@ -71,5 +84,3 @@ def PointsPage(page: ft.Page):
         expand=True,
         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
     )
-
-
--- utils/local_keyboard.py
+++ utils/local_keyboard.py
@@ -4,19 +4,20 @@ import flet as ft
 import pyautogui
 import uuid
 
+
 def listen_keyboard(page: ft.Page, data: list, stop_event: threading.Event):
     rlock = threading.RLock()
     point_idx = 0
 
     def on_key_press(event: keyboard.KeyboardEvent):
         nonlocal point_idx
-        if event.name == "w":
+        if event.name == "insert":
             with rlock:
                 point_idx += 1
                 point = pyautogui.position()
                 data.append([f"point_{point_idx}", str(point.x), str(point.y)])
             page.go(f"/points?refresh={uuid.uuid4()}")
-            
+
     keyboard.on_press(on_key_press)
     while not stop_event.is_set():
         pass
